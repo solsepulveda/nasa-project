cf7614abcb62f23b7ca7d4ba8fac3c2c0ae63055
solsepulveda <solangesepulved@gmail.com>
2024-11-22 19:06:34 -0300

    buttons

--- nasa-api/src/App.jsx
+++ nasa-api/src/App.jsx
@@ -8,29 +8,27 @@ function App() {
   const [savedDate, setSavedDate] = useState('')
   const [selectedImage, setSelectedImage] = useState('')
   const [selectedText, setSelectedText] = useState('')
+  const [yesterday, setYesterday] = useState(1)
 
-  useEffect(() => {
-
-    const getActualDay = () => {
-      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
-      const url = `https://api.nasa.gov/planetary/apod?api_key=${NASA_KEY}`;
-      fetch(url).then(response => response.json())
-        .then(data => {
-          console.log('datos', data); // Muestra los datos en la consola
-          setImage(data.hdurl)
-          setExplanation(data.explanation)
-        })
-        .catch(error => {
-          console.error("Hubo un error al hacer el fetch:", error);
-        });
-    }
-
-    getActualDay()
-
-
-
-    console.log('imagen', image)
-  }, [])
+  /*   useEffect(() => {
+  
+      const getActualDay = () => {
+        const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
+        const url = `https://api.nasa.gov/planetary/apod?api_key=${NASA_KEY}`;
+        fetch(url).then(response => response.json())
+          .then(data => {
+            console.log('datos', data); 
+            setImage(data.hdurl)
+            setExplanation(data.explanation)
+          })
+          .catch(error => {
+            console.error("Hubo un error al hacer el fetch:", error);
+          });
+      }
+  
+      getActualDay()
+  
+    }, []) */
 
   const date = savedDate
   const getAnyDay = () => {
@@ -40,7 +38,7 @@ function App() {
       .then(data => {
         setSelectedImage(data.hdurl)
         setSelectedText(data.explanation)
-        console.log('cierta fecha', data); // Muestra los datos en la consola
+        /* console.log('cierta fecha', data); */ // Muestra los datos en la consola
       })
       .catch(error => {
         console.error("Hubo un error al hacer el fetch:", error);
@@ -51,25 +49,46 @@ function App() {
     getAnyDay()
   }, [savedDate])
 
-  const handleChange=(e)=>{
-setSavedDate(e.target.value)
+
+
+
+
+  const today = new Date();
+  const tomorrow = new Date(today);
+  tomorrow.setDate(today.getDate() - yesterday);
+  //formatear fecha
+  const formattedDate = tomorrow.getFullYear() + "-" +
+    String(tomorrow.getMonth() + 1).padStart(2, '0') + "-" +
+    String(tomorrow.getDate()).padStart(2, '0');
+  console.log(savedDate);
+
+  const handleClickBack = () => {
+    setSavedDate(formattedDate)
+    setYesterday(yesterday + 1)
   }
+
+  const handleClickNext = () => {
+    setSavedDate(formattedDate)
+    setYesterday(yesterday - 1)
+  }
+
+
 
   return (
     <>
       <h1>Astronomy picture of the Day</h1>
-      <div className='daily-image-container'>
+      {/*      <div className='daily-image-container'>
         <img className='daily-image' src={image}></img>
         <p className='explanation'>{explanation}</p>
-      </div>
-{/*       <select onChange={handleChange}>
-        <option value="2011-11-01" >2011-11-01</option>
-        <option value="2022-02-02">2022-02-02</option>
-      </select>
+      </div> */}
       <div className='daily-image-container'>
         <img className='daily-image' src={selectedImage}></img>
         <p className='explanation'>{selectedText}</p>
-      </div> */}
+      </div>
+      <div className='back-next'>
+        <button onClick={handleClickBack}>Back</button>
+       { yesterday >= 1 &&<button onClick={handleClickNext}>Next</button>}
+      </div>
     </>
   )
 }
